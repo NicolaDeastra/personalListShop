18683d7415a464c0cd013821765f0961f684420c
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-05-07 16:56:21 +0700

    setup context api List provider

---
+++ src/containers/List.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import withDataFetching from "../withDataFetching";
+import SubHeader from "../components/Header/SubHeader";
+import ListItem from "../components/ListItem/ListItem";
+
+const ListItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 2% 5%;
+`;
+
+const Alert = styled.span`
+  width: 100%;
+  text-align: center;
+`;
+
+const List = ({ data, loading, error, lists, match, history }) => {
+  const items =
+    data && data.filter((item) => item.listId === parseInt(match.params.id));
+
+  const list =
+    lists && lists.filter((list) => list.id === parseInt(match.params.id));
+
+  return !loading && !error ? (
+    <>
+      {history && list && (
+        <SubHeader
+          goBack={() => history.goBack()}
+          title={list.title}
+          openForm={() => history.push(`${match.url}/new`)}
+        />
+      )}
+      <ListItemWrapper>
+        {items && items.map((item) => <ListItem key={item.id} data={item} />)}
+      </ListItemWrapper>
+    </>
+  ) : (
+    <Alert>{loading ? "Loading..." : error}</Alert>
+  );
+};
+
+export default withDataFetching({
+  dataSource:
+    "https://my-json-server.typicode.com/pranayfpackt/-React-Projects/items",
+})(List);
